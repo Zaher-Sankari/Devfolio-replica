33c57f0f8c85e7937ec52c0b506ad9ce884cf029
zeetonia1 <zaher.lp1@gmail.com>
2025-06-01 13:50:09 +0300

    header done

---
+++ assets/js/text.js
@@ -0,0 +1,13 @@
+var typing = new Typed(".description", {
+    strings: [
+        "CEO DevFolio",
+        "Web Developer",
+        "Web Designer",
+        "Frontend Developer",
+        "Graphics Designer",
+    ],
+    typeSpeed: 100,
+    backSpeed: 40,
+    loop: true,
+});
+    
